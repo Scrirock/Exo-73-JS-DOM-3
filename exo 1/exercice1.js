b8c01fdb02ca30f07ab223bd852bd5ac9e2ee285
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-04 10:09:46 +0100

    jguhikj

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,5 +1,10 @@
 let avatarEls = document.getElementsByTagName("img");
 
+for (let i = 0; i<avatarEls.length; i++){
+    avatarEls[i].style.height = "80px";
+    avatarEls[i].title = avatarEls[i].alt;
+}
+
 
 
 
--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,4 +1,12 @@
+let planete = document.createElement("div");
+planete.className = "planet"
+planete.style.backgroundColor = "blue";
+document.body.append(planete);
 
+let moon = document.createElement("div");
+moon.className = "moon"
+moon.style.backgroundColor = "antiqueWhite";
+planete.append(moon);
 
 
 
